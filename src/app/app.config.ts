6b6667a93bc9e4baa98a8f7cb49443008a185069
Lucas Andrade <lucasilva108@gmail.com>
2025-04-09 23:46:08 -0300

    feat: configure theme as a provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { configThemeInitializerProvider } from './initializers/theme-initializer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    configThemeInitializerProvider,
   ],
 };
